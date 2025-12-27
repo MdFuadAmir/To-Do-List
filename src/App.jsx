1e245b8c85aa0c9f4bc242b7ac4a9b6ee1e3d3b9
Md Fuad Amir <mdfuadamir@gmail.com>
2025-12-27 17:54:48 +0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ToDoList from "./Component/ToDoList/ToDoList";
 
 const App = () => {
   return (
-    <div className="min-h-screen overflow-hidden px-4 py-8 relative">
+    <div className="min-h-screen overflow-hidden relative">
       {/* background */}
       <div
         className="absolute inset-0 bg-cover bg-center"
@@ -15,7 +15,7 @@ const App = () => {
         <img src={logo} alt="to-do" className="w-16 h-16 m-6" />
       </div>
       {/* main */}
-      <div className="relative z-10 min-h-screen flex flex-col items-center justify-center p-4">
+      <div className="relative px-4 py-8 z-10 min-h-screen flex flex-col items-center justify-center p-4">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold text-indigo-400">
             To Do <span className="text-purple-500">List</span>
@@ -25,6 +25,11 @@ const App = () => {
           </p>
         </div>
         <ToDoList />
+      </div>
+      <div className="relative">
+        <p className="text-center text-gray-500 p-4">
+          Copyright © {new Date().getFullYear()} - Md Fuad Amir All right reserved
+        </p>
       </div>
       <Toaster />
     </div>
